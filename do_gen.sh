d69aa914bdffe3343e3dc2494961ef41230541d7
DingYou <dingyou@live.com>
2020-11-23 19:29:01 +0800

    check pymysql is installed and install it when not

--- do_gen.sh
+++ do_gen.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-current_path=`pwd`
+pymysql_version=$(pip3 list | grep -i pymysql | awk '{print $2}')
+if [ "$pymysql_version" ]; then
+  echo "PyMySQL version is $pymysql_version"
+else
+  pip3 install pymysql
+fi
+
+current_path=$(pwd)
 cd "$(dirname "$0")" && python3 class_gen.py
 cd "$current_path" || exit 1
